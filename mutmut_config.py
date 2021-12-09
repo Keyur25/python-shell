795a7c07e9c3b76ece4d1485c9fa4e110dbc7b88
Dinesh <TheTrufth@programmer.net>
2021-12-09 19:24:00 +0000

    Removed redundant variable

--- mutmut_config.py
+++ mutmut_config.py
@@ -1,6 +1,6 @@
 import os.path
 
 def pre_mutation(context):
-    dirname, filename = os.path.split(context.filename)
+    filename = os.path.split(context.filename)[1]
     testfile = "test_" + filename
     context.config.test_command += ' ' + os.path.join("test", testfile)
